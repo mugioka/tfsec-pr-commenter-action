65579c7d49452ea3571f40200c0a282d60198d3b
mugi-o <62197019+mugioka@users.noreply.github.com>
2021-08-29 03:50:46 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,6 @@ set -x
 if [ -n "${GITHUB_WORKSPACE}" ]; then
   cd "${GITHUB_WORKSPACE}" || exit
 fi
-
-if ! tfsec --format=json "${INPUT_WORKING_DIRECTORY}" 2>/dev/null >results.json; then
-  echo "tfsec violations were identified, running commenter..."
-  commenter
-fi
+tfsec --format=json "${INPUT_WORKING_DIRECTORY}" 2>/dev/null >results.json; then
+echo "tfsec violations were identified, running commenter..."
+commenter
