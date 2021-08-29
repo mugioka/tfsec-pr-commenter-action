a01cb637329d9329cd942b46d492211de2d6f637
mugioka <okamugi0722@gmail.com>
2021-08-29 16:52:37 +0900

    Refactor commenter pkg.

--- cmd/commenter/resultsProcessor.go
+++ cmd/commenter/resultsProcessor.go
@@ -1,40 +1,40 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
+  "encoding/json"
+  "io/ioutil"
 )
 
-type checkRange struct {
-	Filename  string `json:"filename"`
-	StartLine int    `json:"start_line"`
-	EndLine   int    `json:"end_line"`
+type CheckRange struct {
+  Filename  string `json:"filename"`
+  StartLine int    `json:"start_line"`
+  EndLine   int    `json:"end_line"`
 }
 
-type result struct {
-	RuleID          string      `json:"rule_id"`
-	LegacyRuleID    string      `json:"legacy_rule_id"`
-	RuleDescription string      `json:"rule_description"`
-	RuleProvider    string      `json:"rule_provider"`
-	Links           []string    `json:"links"`
-	Range           *checkRange `json:"location"`
-	Description     string      `json:"description"`
-	RangeAnnotation string      `json:"-"`
-	Severity        string      `json:"severity"`
+type Result struct {
+  RuleID          string      `json:"rule_id"`
+  LegacyRuleID    string      `json:"legacy_rule_id"`
+  RuleDescription string      `json:"rule_description"`
+  RuleProvider    string      `json:"rule_provider"`
+  Links           []string    `json:"links"`
+  Range           *checkRange `json:"location"`
+  Description     string      `json:"description"`
+  RangeAnnotation string      `json:"-"`
+  Severity        string      `json:"severity"`
 }
 
 const resultsFile = "results.json"
 
-func loadResultsFile() ([]result, error) {
-	results := struct{ Results []result }{}
+func loadResultsFile() ([]Result, error) {
+  results := []Result{}
 
-	file, err := ioutil.ReadFile(resultsFile)
-	if err != nil {
-		return nil, err
-	}
-	err = json.Unmarshal(file, &results)
-	if err != nil {
-		return nil, err
-	}
-	return results.Results, nil
+  file, err := ioutil.ReadFile(resultsFile)
+  if err != nil {
+    return nil, err
+  }
+  err = json.Unmarshal(file, &results)
+  if err != nil {
+    return nil, err
+  }
+  return results, nil
 }
